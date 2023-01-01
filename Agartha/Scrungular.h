187c4a00d4b69b8776c7ce929444bdf808e30a27
DestinyDonut <54757979+PyroNut-69@users.noreply.github.com>
2023-01-01 13:50:16 +0800

    fixed
    scrungular.h is now in the correct directory

---
+++ Agartha/Scrungular.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <windows.h>
+#include <windowsx.h>
+
+#define smain() WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+
+
+// i unironically have no clue how this is working, but it is rn
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+int setWindow(HINSTANCE hInstance, WNDCLASSEX wc);
+
+void createWindow(HINSTANCE lhInstance, HWND hWnd, int lnCmdShow, int width, int height, const wchar_t name[]);
+
+void eventHandlerMaybeIdk(MSG msg);
+
+
+// i dont think this is anything
+/* stupid
+int makeWindow(int W, int H, bool isItWidthBecauseIfItIsMakeItZeroOrFalse)
+{
+    int WH[2] = {W, H};
+    if (isItWidthBecauseIfItIsMakeItZeroOrFalse == 0)
+        return WH[0];
+    else
+        return H;
+}
+*/
